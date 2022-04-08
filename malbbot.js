71b0038c510b611bd56de17ee24a4f5a2fce623b
LeoLittig <leonardolittig@gmail.com>
2022-04-07 22:37:56 -0300

    malbbot: adiciona algumas frases #6

--- malbbot.js
+++ malbbot.js
@@ -7,15 +7,15 @@ var frasePronta;
 
     //inicio
 
-var inicios = ["tava no cursinho falando d", "bah, hoje eu vi ", "yall, eu tava pensando n", "literalmente me sentindo "];
+var inicios = ["tava no cursinho falando d", "bah, hoje eu vi ", "yall, eu tava pensando n", "literalmente me sentindo ", "yall! vós ouviveis que "];
 
     //meio
 
-var meios = ["a sandrinha", "o carro da sandrinha", "a mommy ufrgs", "o thiago berto", "as casas da italia", "a wine night"];
+var meios = ["a sandrinha", "o carro da sandrinha", "a mommy ufrgs", "o thiago berto", "as casas da italia", "a wine night", "o peso 1", "do peso 3", "o vestibular", "o potao"];
 
     //fim
 
-var fins = [" com o doutor leonardo.", " com a doutora gabrielle.", " com a sandrinha.", "... bahhhhhh..", "... literalmente.", " neah", ".. ainda bem que é peso 1", "... o foda é que é peso 3..."];
+var fins = [" com o doutor leonardo.", " com a doutora gabrielle.", " com a sandrinha.", "... bahhhhhh..", "... literalmente.", " neah", ".. ainda bem que é peso 1", "... o foda é que é peso 3...", "... matar-me-ei"];
 
 var fazerFrase = function () {
     var inicio = inicios[Math.floor(Math.random() * inicios.length)];
